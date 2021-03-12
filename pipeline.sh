8762f9bd8605495cd2e7dda69c7edb2bed14ac47
PeterDeWeirdt <petedeweirdt@gmail.com>
2021-03-12 12:22:30 -0500

    first commit

---
+++ pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python notebooks/00_processing_Aguirre2017.ipynb
+python notebooks/00_processing_Chari2015.ipynb
+python notebooks/00_processing_DeWeirdt2020.ipynb
+python notebooks/00_processing_DeWeirdt2020.ipynb
+python notebooks/00_processing_Doench2014.ipynb
+python notebooks/00_processing_Doench2016.ipynb
+python notebooks/00
